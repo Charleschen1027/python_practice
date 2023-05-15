c7e505ddb721cdef1a98daca51f923b0478133e5
Charleschen1027 <chenhr@dataui.cn>
2023-05-15 22:15:45 +0800

    学习函数定义

--- practice.py
+++ practice.py
@@ -1,2 +1,22 @@
-import random
-print(random.choice([1,3,5]))
+# import random
+# print(random.choice([1,3,5]))
+
+#函数的定义和调用
+# def sayHello():
+#     print("Hello world!")
+#
+# sayHello()
+
+# 创建一个函数 func，有一个名为 param 的参数
+# 在函数中，将输入参数的数值乘以2，作为返回值
+# def func(param):
+#     param*=2
+#     return param
+#
+# p = func(20)
+# print(p)
+#定义带参数的函数
+def sayHello(someone):
+    print(someone,'says Hello!')
+
+sayHello('Charles')
